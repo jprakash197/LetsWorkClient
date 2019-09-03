73f6e7c974abd0efb9175af71bba3af40539def2
Mahima Hegde <M1053894@mindtree.com>
2019-09-03 12:30:15 +0530

    added environments in angular

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
-import { HeaderComponent } from '../core/shell/header/header.component';
-import { FooterComponent } from '../core/shell/footer/footer.component';
-import { SigninComponent } from '../core/authentication/signin/signin.component';
-import { SignupComponent } from '../core/authentication/signup/signup.component';
+import { HeaderComponent } from './shell/header/header.component';
+import { FooterComponent } from './shell/footer/footer.component';
+import { SigninComponent } from './authentication/signin/signin.component';
+import { SignupComponent } from './authentication/signup/signup.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { ShellComponent } from './shell/shell.component';
-import { ResetPasswordComponent } from '../core/authentication/reset-password/reset-password.component';
+import { ResetPasswordComponent } from './authentication/reset-password/reset-password.component';
 
 
 @NgModule({
--- src/app/venues/venues.module.ts
+++ src/app/venues/venues.module.ts
@@ -4,12 +4,12 @@ import { CommonModule } from '@angular/common';
 import { VenuesRoutingModule } from './venues-routing.module';
 import { ResultsPageComponent } from './results-page/results-page.component';
 import { DetailsPageComponent } from './details-page/details-page.component';
-import { BookNowComponent } from '../venues/details-page/book-now/book-now.component';
-import { ReadDetailsComponent } from '../venues/details-page/read-details/read-details.component';
-import { FilterVenuesComponent } from '../venues/results-page/filter-venues/filter-venues.component';
-import { SortVenuesComponent } from '../venues/results-page/sort-venues/sort-venues.component';
-import { GetMoreDetailsComponent } from '../venues/results-page/get-more-details/get-more-details.component';
-import { MapComponent } from '../venues/details-page/map/map.component';
+import { BookNowComponent } from './details-page/book-now/book-now.component';
+import { ReadDetailsComponent } from './details-page/read-details/read-details.component';
+import { FilterVenuesComponent } from './results-page/filter-venues/filter-venues.component';
+import { SortVenuesComponent } from './results-page/sort-venues/sort-venues.component';
+import { GetMoreDetailsComponent } from './results-page/get-more-details/get-more-details.component';
+import { MapComponent } from './details-page/map/map.component';
 
 
 @NgModule({
