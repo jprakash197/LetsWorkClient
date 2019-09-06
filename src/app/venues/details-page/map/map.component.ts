6b20773e84d252822cb857dfca8a89125103fab8
ZachMM <zachary.mcconnell@mindtree.com>
2019-09-06 15:07:24 +0530

    Add geolocation services and fallback option \ Add smarter way to picking locaiton using geoservices

--- src/app/venues/details-page/map/map.component.ts
+++ src/app/venues/details-page/map/map.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
 
 import { } from 'googlemaps';
+import { DeclareFunctionStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-map',
@@ -8,6 +10,7 @@ import { } from 'googlemaps';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  map: google.maps.Map;
 
   constructor() { }
 
@@ -17,16 +20,40 @@ export class MapComponent implements OnInit {
 
   // Initialize and add the map
   initMap() {
-    var userLat = 12.9;
-    var userLong = 77.3;
-
     // The location of Bengaluru
-    var latLongLoc = { lat: userLat, lng: userLong };
+    var bengaluru = { lat: 12.9, lng: 77.3 };
     // The map, centered at Bengaluru
-    var map = new google.maps.Map(
-      document.getElementById('map'), { zoom: 4, center: latLongLoc });
-    // The marker, positioned at Bengaluru
-    var marker = new google.maps.Marker({ position: latLongLoc, map: map });
+    this.map = new google.maps.Map(
+      document.getElementById('map'), { zoom: 4, center: bengaluru });
+
+    const infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        let pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(this.map);
+        this.map.setCenter(pos);
+        const marker = new google.maps.Marker({ position: bengaluru, map: this.map });
+      }, () => {
+        this.handleLocationError(true, infoWindow, this.map.getCenter());
+      });
+    } else {
+      this.handleLocationError(false, infoWindow, bengaluru);
+    }
+  }
+
+  handleLocationError(browserHasGeolocaiton, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocaiton
+      ? 'Error: Geolocation service failed'
+      : 'Error: Browser does not support geolocation');
+    infoWindow.open(this.map)
   }
 
 }
