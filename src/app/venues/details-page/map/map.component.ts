375c60826e5344449cd6366fdf2193e7609fe87d
ZachMM <zachary.mcconnell@mindtree.com>
2019-09-06 14:38:07 +0530

    Add maps component to Venue Results component sidebar

--- src/app/venues/details-page/map/map.component.ts
+++ src/app/venues/details-page/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { } from 'googlemaps';
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -10,6 +12,21 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.initMap();
+  }
+
+  // Initialize and add the map
+  initMap() {
+    var userLat = 12.9;
+    var userLong = 77.3;
+
+    // The location of Bengaluru
+    var latLongLoc = { lat: userLat, lng: userLong };
+    // The map, centered at Bengaluru
+    var map = new google.maps.Map(
+      document.getElementById('map'), { zoom: 4, center: latLongLoc });
+    // The marker, positioned at Bengaluru
+    var marker = new google.maps.Marker({ position: latLongLoc, map: map });
   }
 
 }
